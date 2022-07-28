f797d0af7e5b2a4f987b1523274e3224706e5aa7
GonzaloQuintana <quintanagonzalodaniel@gmail.com>
2022-07-28 13:18:02 -0300

    Segunda entrega + Librerias

--- carrito.js
+++ carrito.js
@@ -1,12 +1,18 @@
 let btn_compra = document.querySelectorAll(".botonCompra");
 
+console.log(btn_compra);
+
 for( let boton of btn_compra){
 
     boton.addEventListener("click" , agregar_a_carrito);
 
 }
 
+
+
 let carrito = [];
+
+
 
 function agregar_a_carrito(e){
 
@@ -17,62 +23,71 @@ function agregar_a_carrito(e){
     let abuelo = padre.parentNode;
     //console.log( padre );
 
-    let nombre_producto = padre.querySelectorAll("h4").textContent;
-    //console.log(nombre_producto);
+    let nombre_producto = padre.querySelector("h4").textContent;
+    //console.log( nombre_producto);
 
-    let precio = padre.querySelectorAll("span").textContent;
-    //console.log(precio);
+    let precio = padre.querySelector("span").textContent;
+    //console.log( precio);
 
-
-    let imagen = abuelo.querySelectorAll("img").src;
-    //console.log(imagen);
+    let img = abuelo.querySelector("img").src;
+    //console.log( img);
 
     let producto = {
 
         nombre: nombre_producto,
-        img: imagen,
+        img: img,
         precio: precio,
         cantidad: 1
 
     };
+
+    Toastify({
+        text:"Agregado al carrito",
+        duration: 1000,
+        style:{
+            background:"pink",
+            color:"black"
+        } 
+    }).showToast();
 
     carrito.push(producto);
 
     let arreglo_JSON = JSON.stringify(carrito);
     localStorage.setItem("carrito" , arreglo_JSON);
     
-    console.log( producto);
+    console.log(carrito);
 
-//    mostrar_carrito( producto);
+    mostrar_carrito( producto);
 }
 
 
-    //function mostrar_carrito( producto){
+    function mostrar_carrito( producto){
 
-        //let fila = document.createElement("tr");
+        let fila = document.createElement("tr");
 
-        //fila.innerHTML = '<td><img src="{$producto.img}"></td>
-        //                  <td>${producto.nombre}</td>
-        //                  <td>${producto.cantidad}</td>
-        //                  <td>${producto.precio}</td>'
+        fila.innerHTML = `<td><img src="{$producto.img}"></td>
+                          <td>${producto.nombre}</td>
+                          <td>${producto.cantidad}</td>
+                          <td>${producto.precio}</td>
+                          <td><button class="btn-danger borrar_elemento">Borrar</button></td>`;
 
-    //let tabla = document.getElementById("tbody");
+    let tabla = document.getElementById("tbody");
 
-    //tabla.append(fila);
+    tabla.append(fila);
       
-    //let botones_borrar = document.querySelectorAll(".borrar_elemento");
+    let botones_borrar = document.querySelectorAll(".borrar_elemento");
     
-    //for( let boton of botones_borrar){
+    for( let boton of botones_borrar){
 
-    //    boton.addEventListener("click" , borrar_producto);
+        boton.addEventListener("click" , borrar_producto);
     
-    //}
-//}
+    }
+}
 
-//function borrar_producto(e){
+function borrar_producto(e){
 
-    //let abuelo = e.target.parentNode.parentNode;
-    //abuelo.remove();
+    let abuelo = e.target.parentNode.parentNode;
+    abuelo.remove();
 
 
-//}
+}
---
+++ librerias.js
@@ -0,0 +1,19 @@
+/*
+Swal.fire({
+    icon:"error",
+    title:"ERROR 404",
+    text:"No se encontro el sitio",
+    footer:""
+});
+*/
+
+/*Toastify({
+    text:"Agregado al carrito",
+    duration: 1000,
+    style:{
+        background:"pink",
+        color:"black"
+    } 
+}).showToast();
+*/
+
